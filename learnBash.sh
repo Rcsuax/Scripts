194434904fd0a04526a41e470a92872bd548b974
Rcsuax <rcsuax@gmail.com>
2017-03-14 22:50:19 +0000

    added some additonal information

--- learnBash.sh
+++ learnBash.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
-## Double Hashtag means this line is a comment
-##  #!/usr/bin/env bash is a shebang and defines enviroment to run the script
+
+#  Hashtag means this line is a comment
+#  #!/usr/bin/env bash is a shebang and defines enviroment to run the script
+
+<<'COMMENT'
+
+This is a multi line comment
+
+COMMENT
 
 echo "hello world"
 
-## chmod +x makes the file an executable
+# chmod +x makes the file an executable
 
 
