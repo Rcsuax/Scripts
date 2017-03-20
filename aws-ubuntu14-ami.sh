aef25f815e78341e6a6fe8285e36060fb7726535
Rcsuax <rcsuax@gmail.com>
2017-03-20 16:08:05 +0000

    find latest ubuntu aws ami image

---
+++ aws-ubuntu14-ami.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+name=$(\
+    aws --region us-west-2 ec2 describe-images --owners 099720109477 \
+        --filters Name=root-device-type,Values=ebs \
+            Name=architecture,Values=x86_64 \
+            Name=name,Values='*hvm-ssd/ubuntu-trusty-14.04*' \
+    | awk -F ': ' '/"Name"/ { print $2 | "sort" }' \
+    | tr -d '",' | tail -1)
+
+ami_id=$(\
+    aws --region us-west-2 ec2 describe-images --owners 099720109477 \
+        --filters Name=name,Values="$name" \
+    | awk -F ': ' '/"ImageId"/ { print $2 }' | tr -d '",')
+
+
+echo "$name"
+echo "$ami_id"
